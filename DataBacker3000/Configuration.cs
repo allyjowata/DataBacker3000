310e8df2559d7e31401ad5248993d6a1077da104
allyjowata <ally.jowata@ceridian.com>
2022-08-23 11:38:40 +0400

    Add project files.

---
+++ DataBacker3000/Configuration.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DataBacker3000
+{
+    internal class Configuration
+    {
+        public Dictionary<String, bool> configDictionary = new Dictionary<String, bool>();
+
+
+        public Configuration()
+        {
+            ImportConfig();
+            if (IsFirstRun())
+            {
+                CreateTask();
+                configDictionary["firstRun"] = false;
+                ExportConfig();
+            }
+        }
+
+        public void ImportConfig()
+        {
+            var file = new StreamReader("config.txt");
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                var keyValue = line.Split('=');
+                bool value = keyValue[1].CompareTo("True") == 0 ? true : false;
+                configDictionary.Add(keyValue[0], value);
+            }
+            file.Close();
+        }
+
+        public void ExportConfig()
+        {
+            File.WriteAllLines("config.txt", configDictionary.Select(x => x.Key + "=" + x.Value.ToString()).ToArray());
+        }
+
+        public bool IsFirstRun()
+        {
+            return configDictionary["firstRun"];
+        }
+
+        public void CreateTask()
+        {
+            string cmdText;
+            //cmdText = "/K SCHTASKS /CREATE /SC DAILY /TN \"DataBacker3000\\DatabaseBackup\" /TR \"C: \\Users\\p129fb7\\source\\repos\\DataBacker3000\\DataBacker3000\\bin\\Release\\DataBacker3000.exe\" /ST 00:42";
+            cmdText = "/C SCHTASKS /CREATE /SC DAILY /TN \"DataBacker3000\\DatabaseBackup\" /TR \"C: \\DataBacker3000\\DataBacker3000.exe\" /ST %TIME:~-11,5%";
+            System.Diagnostics.Process.Start(@"C:\Windows\system32\cmd.exe", cmdText);
+        }
+
+
+    }
+}
---
+++ DataBacker3000/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DataBacker3000
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=.;Integrated Security=SSPI;Pooling=False";
+            string pathFolder1 = $@"C:\DataBacker3000\DBBackups\Folder1";
+            string pathFolder2 = $@"C:\DataBacker3000\DBBackups\Folder2";
+            Configuration configuration = new Configuration();
+            BackupService backupFolder1 = new BackupService(connectionString, pathFolder1);
+            BackupService backupFolder2 = new BackupService(connectionString, pathFolder2);
+
+            if (configuration.configDictionary["folder1"] == false && configuration.configDictionary["folder2"] == false)
+            {
+                backupFolder1.BackupAllUserDatabases();
+                backupFolder2.BackupAllUserDatabases();
+                configuration.configDictionary["folder2"] = true;
+            }
+            else
+            {
+                if (configuration.configDictionary["folder1"] == false)
+                {
+                    backupFolder1.BackupAllUserDatabases();
+                    configuration.configDictionary["folder1"] = true;
+                    configuration.configDictionary["folder2"] = false;
+                }
+                else
+                {
+                    backupFolder2.BackupAllUserDatabases();
+                    configuration.configDictionary["folder1"] = false;
+                    configuration.configDictionary["folder2"] = true;
+                }
+            }
+            configuration.ExportConfig();
+
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("DatabaseBacker3000 success!!!!");
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+
+        }
+    }
+
+}
